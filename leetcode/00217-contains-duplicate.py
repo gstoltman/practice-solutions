cffe00d0550c930bd51f999534e4a38eac8b81ff
gstoltman <gstoltman@gmail.com>
2023-01-30 15:38:57 -0800

    Update 00217-contains-duplicate.py

--- leetcode/00217-contains-duplicate.py
+++ leetcode/00217-contains-duplicate.py
@@ -1,8 +1,8 @@
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        mylist = []
+        myset = set()
         for n in nums:
             if n in mylist:
                 return True
-            mylist.append(n)
+            myset.add(n)
         return False
